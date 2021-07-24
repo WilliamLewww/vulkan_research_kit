f4b8ba564b5e8c17b8e5daec689bebed55aec143
William Lew <WilliamMilesLew@gmail.com>
2021-07-23 23:48:37 -0700

    Added device test for adding extensions

--- tests/device_test.cpp
+++ tests/device_test.cpp
@@ -192,6 +192,55 @@ TEST (DeviceTest, AddAllQueues) {
   delete instance;
 }
 
+TEST (DeviceTest, AddAllExtensions) {
+  Instance* instance = new Instance();
+  instance->activate();
+  EXPECT_NE(*instance->getInstanceHandlePtr(), VK_NULL_HANDLE);
+
+  std::vector<VkPhysicalDevice> deviceHandleList =
+      Device::getPhysicalDevices(instance->getInstanceHandlePtr());
+
+  VkPhysicalDevice activePhysicalDevice;
+  for (VkPhysicalDevice deviceHandle : deviceHandleList) {
+    VkPhysicalDeviceProperties physicalDeviceProperties =
+        Device::getPhysicalDeviceProperties(&deviceHandle);
+
+    if (physicalDeviceProperties.deviceType ==
+        VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+      activePhysicalDevice = deviceHandle;
+      break;
+    }
+  }
+
+  std::vector<VkQueueFamilyProperties> queueFamilyPropertiesList =
+      Device::getQueueFamilyPropertiesList(&activePhysicalDevice);
+
+  uint32_t queueFamilyIndex = -1;
+  for (uint32_t x = 0; x < queueFamilyPropertiesList.size(); x++) {
+    if (queueFamilyPropertiesList[x].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+      queueFamilyIndex = x;
+    }
+  }
+
+  Device* device = new Device(instance->getInstanceHandlePtr(),
+      &activePhysicalDevice, queueFamilyIndex, 1);
+
+  std::vector<VkExtensionProperties> extensionPropertiesList =
+      device->getAvailableExtensionPropertiesList();
+
+  for (VkExtensionProperties extensionProperties : extensionPropertiesList) {
+    device->addExtension(extensionProperties.extensionName);
+  }
+
+  device->activate();
+
+  EXPECT_NE(*device->getDeviceHandlePtr(), VK_NULL_HANDLE);
+  EXPECT_NE(*device->getQueueHandlePtr(queueFamilyIndex, 0), VK_NULL_HANDLE);
+
+  delete device;
+  delete instance;
+}
+
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
