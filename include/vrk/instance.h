dd3159b3c93d299bf119fba6458d55b5d0757856
William Lew <WilliamMilesLew@gmail.com>
2021-08-11 17:41:39 -0700

    Removed all files

--- include/vrk/instance.h
+++ include/vrk/instance.h
@@ -1,67 +1,9 @@
 #pragma once
 
-#include "vrk/helper.h"
-#include "vrk/component.h"
-
-#include <vulkan/vulkan.h>
-
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <stdlib.h>
-#include <string.h>
-
-class Instance : public Component {
-private:
-  VkInstance instanceHandle;
-  VkDebugUtilsMessengerEXT debugUtilsMessengerHandle;
-
-  uint32_t majorVersion;
-  uint32_t minorVersion;
-  uint32_t patchVersion;
-
-  VkValidationFeaturesEXT validationFeatures;
-  VkDebugUtilsMessengerCreateInfoEXT debugUtilsMessengerCreateInfo;
-  VkApplicationInfo applicationInfo;
-
-  std::vector<VkValidationFeatureEnableEXT> enabledValidationFeatureList;
-  std::vector<VkValidationFeatureDisableEXT> disabledValidationFeatureList;
-
-  std::vector<VkLayerProperties> layerPropertiesList;
-  std::vector<VkExtensionProperties> extensionPropertiesList;
-
-  std::vector<std::string> enabledLayerNameList;
-  std::vector<std::string> enabledExtensionNameList;
+class Instance {
 public:
   Instance();
 
   ~Instance();
-
-  std::string getVulkanVersionAPI();
-
-  void addValidationFeatureEnable(
-      VkValidationFeatureEnableEXT validationFeatureEnable);
-
-  void addValidationFeatureDisable(
-      VkValidationFeatureDisableEXT validationFeatureDisable);
-
-  void setDebugUtilsMessageSeverityFlagBits(
-      VkDebugUtilsMessageSeverityFlagBitsEXT debugUtilsMessageSeverityFlagBits);
-
-  void setDebugUtilsMessageTypeFlagBits(
-      VkDebugUtilsMessageTypeFlagBitsEXT debugUtilsMessageTypeFlagBitsEXT);
-
-  std::vector<VkLayerProperties> getAvailableLayerPropertiesList();
-
-  bool addLayer(std::string layerName);
-
-  std::vector<VkExtensionProperties> getAvailableExtensionPropertiesList(
-      std::string layerName = "");
-
-  bool addExtension(std::string extensionName, std::string layerName = "");
-
-  bool activate();
-
-  VkInstance* getInstanceHandlePtr();
+private:
 };
