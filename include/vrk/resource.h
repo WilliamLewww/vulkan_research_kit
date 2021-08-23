926c7332827a403bac5eb266e3439c235cf4d7ed
William Lew <WilliamMilesLew@gmail.com>
2021-08-22 23:55:44 -0700

    Added resource finder

--- include/vrk/resource.h
+++ include/vrk/resource.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <fstream>
+#include <cstdlib>
 
 class Resource {
 public:
--- src/resource.cpp
+++ src/resource.cpp
@@ -1,1 +1,18 @@
 #include "vrk/resource.h"
+
+std::string Resource::findResource(std::string resourcePath) {
+  std::ifstream f = std::ifstream(resourcePath.c_str());
+  if (f.good()) {
+    return resourcePath.c_str();
+  }
+  else if (std::getenv("VRK_RESOURCE_PATH") != NULL) {
+    std::string envResourcePath = std::getenv("VRK_RESOURCE_PATH");
+
+    f = std::ifstream(envResourcePath + resourcePath);
+    if (f.good()) {
+      return envResourcePath + resourcePath;
+    }
+  }
+
+  return "";
+}
