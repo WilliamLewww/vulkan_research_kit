649b3e0bfdb1c628a679355e9938f2d9f709c9ab
William Lew <WilliamMilesLew@gmail.com>
2021-08-26 00:30:06 -0700

    Added ray tracing library

---
+++ include/vrk/ray_tracing/acceleration_structure.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "vrk/helper.h"
+
+#include <vulkan/vulkan.h>
+
+class AccelerationStructure {
+public:
+  AccelerationStructure();
+
+  ~AccelerationStructure();
+private:
+};
---
+++ src/ray_tracing/acceleration_structure.cpp
@@ -0,0 +1,9 @@
+#include "vrk/ray_tracing/acceleration_structure.h"
+
+AccelerationStructure::AccelerationStructure() {
+
+}
+
+AccelerationStructure::~AccelerationStructure() {
+
+}
