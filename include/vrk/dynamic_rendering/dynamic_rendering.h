2dff389b517278190cd3ca58cd039e753122d627
William Lew <WilliamMilesLew@gmail.com>
2021-11-11 21:57:50 -0800

    Implemented dynamic rendering

--- include/vrk/dynamic_rendering/dynamic_rendering.h
+++ include/vrk/dynamic_rendering/dynamic_rendering.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <vulkan/vulkan.h>
+
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -10,3 +13,29 @@ const std::vector<std::string>
 const std::vector<std::string>
     DYNAMIC_RENDERING_REQUIRED_DEVICE_EXTENSION_LIST = {
         "VK_KHR_dynamic_rendering"};
+
+namespace DynamicRendering {
+struct RenderingAttachmentInfoParam {
+  VkImageView imageViewHandle;
+  VkImageLayout imageLayout;
+  VkResolveModeFlagBits resolveModeFlagBits;
+  VkImageView resolveImageViewHandle;
+  VkImageLayout resolveImageLayout;
+  VkAttachmentLoadOp attachmentLoadOp;
+  VkAttachmentStoreOp attachmentStoreOp;
+  VkClearValue clearValue;
+};
+
+static void beginRenderingCmd(VkCommandBuffer &commandBufferHandleRef,
+                              VkRenderingFlagsKHR renderingFlags,
+                              VkRect2D rect2DRenderArea, uint32_t layerCount,
+                              uint32_t viewMask, uint32_t colorAttachmentCount,
+                              std::vector<RenderingAttachmentInfoParam>
+                                  colorRenderingAttachmentInfoParamList,
+                              std::shared_ptr<RenderingAttachmentInfoParam>
+                                  depthRenderingAttachmentInfoParamPtr,
+                              std::shared_ptr<RenderingAttachmentInfoParam>
+                                  stencilRenderingAttachmentInfoParamPtr);
+
+static void endRenderingCmd(VkCommandBuffer &commandBufferHandleRef);
+} // namespace DynamicRendering
