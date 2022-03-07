164abb85fbebd3dff2a77b293052dd7c4e25c625
William Lew <WilliamMilesLew@gmail.com>
2022-03-06 21:30:59 -0800

    Fixed resource paths

--- src/resource.cpp
+++ src/resource.cpp
@@ -4,13 +4,20 @@ std::string Resource::findResource(std::string resourcePath) {
   std::ifstream f = std::ifstream(resourcePath.c_str());
   if (f.good()) {
     return resourcePath.c_str();
-  } else if (std::getenv("VRK_RESOURCES_PATH") != NULL) {
-    std::string envResourcePath = std::getenv("VRK_RESOURCES_PATH");
+  } else if (std::getenv("VRK_RESOURCES_DIR") != NULL) {
+    std::string envResourcePath = std::getenv("VRK_RESOURCES_DIR");
 
     f = std::ifstream(envResourcePath + resourcePath);
     if (f.good()) {
       return envResourcePath + resourcePath;
     }
+  } else {
+    #ifdef VRK_RESOURCES_DIR
+    f = std::ifstream(VRK_RESOURCES_DIR + resourcePath);
+    if (f.good()) {
+      return VRK_RESOURCES_DIR + resourcePath;
+    }
+    #endif
   }
 
   return "";
